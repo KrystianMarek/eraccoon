25ba079ae237c386d257b6fd0b762fc87e2241f7
Krystian Marek <kmarek@nvidia.com>
2024-09-28 23:56:07 +0200

    update

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,63 +1,65 @@
  #include "CytronMotorDriver.h"
 
 // Define Joystick pins
-const int J_FORWARD = 22;
-const int J_BACK = 25;
-const int J_LEFT = 24;
-const int J_RIGHT = 23;
+const int PIN_J_FORWARD = 22;
+const int PIN_J_BACK = 25;
+const int PIN_J_LEFT = 24;
+const int PIN_J_RIGHT = 23;
 
 const int MAX_SPEED = 128;
 const int MIN_SPEED = 50;
 
 
-// Define the motor driver pins
-CytronMD motor_fl(PWM_PWM, 2, 3);   
-CytronMD motor_rl(PWM_PWM, 4, 5);
-CytronMD motor_fr(PWM_PWM, 6, 7);   
-CytronMD motor_rr(PWM_PWM, 8, 9);
+void setup_motors() {
+  // Define the motor driver pins
+  CytronMD motor_fl(PWM_PWM, 2, 3);   
+  CytronMD motor_rl(PWM_PWM, 4, 5);
+  CytronMD motor_fr(PWM_PWM, 6, 7);   
+  CytronMD motor_rr(PWM_PWM, 8, 9);
+}
 
-// The setup routine runs once when you press reset.
-
-void init_joystick() {
+void setup_joystick() {
   for(int i=22;i<26;i++)
     pinMode(i,INPUT);
 }
 
+// The setup routine runs once when you press reset.
 void setup() {
-  init_joystick();
+  setup_motors();
+  setup_joystick();
 }
 
-void j_ride(int jForward, int jBackward, int jLeft, int jRight) {
+void j_ride(int j_forward, int j_backward, int j_left, int j_right) {
   //stop
-  if (jForward == LOW && jBackward == LOW && jLeft == LOW && jRight == LOW) {
+  if (j_forward == LOW && j_backward == LOW && j_left == LOW && j_right == LOW) {
     motor_fl.setSpeed(0);
     motor_fr.setSpeed(0);
     motor_rl.setSpeed(0);
     motor_rr.setSpeed(0);
   }
   //forward
-  if (jForward == HIGH && jBackward == LOW && jLeft == LOW && jRight == LOW) {
+  if (j_forward == HIGH && j_backward == LOW && j_left == LOW && j_right == LOW) {
     motor_fl.setSpeed(MIN_SPEED);
     motor_fr.setSpeed(MIN_SPEED);
     motor_rl.setSpeed(MIN_SPEED);
     motor_rr.setSpeed(MIN_SPEED);
   }
   //back
-  if (jForward == LOW && jBackward == HIGH && jLeft == LOW && jRight == LOW) {
+  if (j_forward == LOW && j_backward == HIGH && j_left == LOW && j_right == LOW) {
     motor_fl.setSpeed(-MIN_SPEED);
     motor_fr.setSpeed(-MIN_SPEED);
     motor_rl.setSpeed(-MIN_SPEED);
     motor_rr.setSpeed(-MIN_SPEED);
   }
   //dead left 
-  if (jForward == LOW && jBackward == LOW && jLeft == HIGH && jRight == LOW) {
+  if (j_forward == LOW && j_backward == LOW && j_left == HIGH && j_right == LOW) {
     motor_fl.setSpeed(-MIN_SPEED);
     motor_fr.setSpeed(MIN_SPEED);
     motor_rl.setSpeed(-MIN_SPEED);
     motor_rr.setSpeed(MIN_SPEED);
   }
   //dead right
-  if (jForward == LOW && jBackward == LOW && jLeft == LOW && jRight == HIGH) {
+  if (j_forward == LOW && j_backward == LOW && j_left == LOW && j_right == HIGH) {
     motor_fl.setSpeed(MIN_SPEED);
     motor_fr.setSpeed(-MIN_SPEED);
     motor_rl.setSpeed(MIN_SPEED);
@@ -65,9 +67,7 @@ void j_ride(int jForward, int jBackward, int jLeft, int jRight) {
   }
 }
 
-
-
 // The loop routine runs over and over again forever.
 void loop() {
-  j_ride(digitalRead(J_FORWARD), digitalRead(J_BACK), digitalRead(J_LEFT), digitalRead(J_RIGHT));
+  j_ride(digitalRead(PIN_J_FORWARD), digitalRead(PIN_J_BACK), digitalRead(PIN_J_LEFT), digitalRead(PIN_J_RIGHT));
 }
--- readme.md
+++ readme.md
@@ -8,3 +8,10 @@
 - Remote control
 - Battery capacity meter and safety off
 - https://developer.nvidia.com/embedded/learn/get-started-jetson-nano-devkit
+
+# Arduino
+## libs
+- https://github.com/CytronTechnologies/CytronMotorDriver
+
+## Background
+- https://www.instructables.com/Understanding-the-Pull-up-Resistor-With-Arduino/
