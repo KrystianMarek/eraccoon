1de8195695ef8d7d2b8d6b499d7647f82279c565
Krystian Marek <kmarek@nvidia.com>
2024-09-29 22:27:54 +0200

    update

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,4 +1,5 @@
  #include "CytronMotorDriver.h"
+ #include "DFRobot_RGBLCD1602.h"
 
 // Define Joystick pins
 const int PIN_J_FORWARD = 22;
@@ -9,23 +10,31 @@ const int PIN_J_RIGHT = 23;
 const int MAX_SPEED = 128;
 const int MIN_SPEED = 50;
 
+// LCD
+const int colorR = 0;
+const int colorG = 128;
+const int colorB = 0;
 
-void setup_motors() {
-  // Define the motor driver pins
-  CytronMD motor_fl(PWM_PWM, 2, 3);   
-  CytronMD motor_rl(PWM_PWM, 4, 5);
-  CytronMD motor_fr(PWM_PWM, 6, 7);   
-  CytronMD motor_rr(PWM_PWM, 8, 9);
-}
+// Define the motor driver pins
+CytronMD motor_fl(PWM_PWM, 2, 3);   
+CytronMD motor_rl(PWM_PWM, 4, 5);
+CytronMD motor_fr(PWM_PWM, 6, 7);   
+CytronMD motor_rr(PWM_PWM, 8, 9);
+
+DFRobot_RGBLCD1602 lcd(0x2D, 16, 2);
 
 void setup_joystick() {
   for(int i=22;i<26;i++)
     pinMode(i,INPUT);
 }
 
-// The setup routine runs once when you press reset.
 void setup() {
-  setup_motors();
+  lcd.init();
+  lcd.clear();
+  lcd.setRGB(colorR, colorG, colorB);
+    
+  // Print a message to the LCD.
+  lcd.print("hello!");
   setup_joystick();
 }
 
@@ -71,3 +80,68 @@ void j_ride(int j_forward, int j_backward, int j_left, int j_right) {
 void loop() {
   j_ride(digitalRead(PIN_J_FORWARD), digitalRead(PIN_J_BACK), digitalRead(PIN_J_LEFT), digitalRead(PIN_J_RIGHT));
 }
+
+/*
+
+const int DESIRED_SPEED = 255; // Maximum motor speed
+const unsigned long ACCELERATION_TIME = 1000; // Time to reach full speed in milliseconds
+
+unsigned long lastUpdateTime = 0;
+int currentSpeed = 0;
+
+void controlMotors() {
+  unsigned long currentTime = millis();
+  unsigned long deltaTime = currentTime - lastUpdateTime;
+  lastUpdateTime = currentTime;
+
+  bool forward = digitalRead(J_FORWARD);
+  bool backward = digitalRead(J_BACKWARD);
+  bool left = digitalRead(J_LEFT);
+  bool right = digitalRead(J_RIGHT);
+
+  int targetSpeed = 0;
+  if (forward || backward || left || right) {
+    targetSpeed = DESIRED_SPEED;
+  }
+
+  // Calculate speed change
+  int speedChange = (int)((float)DESIRED_SPEED * deltaTime / ACCELERATION_TIME);
+  
+  // Accelerate or decelerate
+  if (targetSpeed > currentSpeed) {
+    currentSpeed = min(currentSpeed + speedChange, DESIRED_SPEED);
+  } else if (targetSpeed < currentSpeed) {
+    currentSpeed = max(currentSpeed - speedChange, 0);
+  }
+
+  // Set motor speeds based on joystick input
+  if (forward) {
+    motor_fl.setSpeed(currentSpeed);
+    motor_fr.setSpeed(currentSpeed);
+    motor_rl.setSpeed(currentSpeed);
+    motor_rr.setSpeed(currentSpeed);
+  } else if (backward) {
+    motor_fl.setSpeed(-currentSpeed);
+    motor_fr.setSpeed(-currentSpeed);
+    motor_rl.setSpeed(-currentSpeed);
+    motor_rr.setSpeed(-currentSpeed);
+  } else if (left) {
+    motor_fl.setSpeed(-currentSpeed);
+    motor_fr.setSpeed(currentSpeed);
+    motor_rl.setSpeed(-currentSpeed);
+    motor_rr.setSpeed(currentSpeed);
+  } else if (right) {
+    motor_fl.setSpeed(currentSpeed);
+    motor_fr.setSpeed(-currentSpeed);
+    motor_rl.setSpeed(currentSpeed);
+    motor_rr.setSpeed(-currentSpeed);
+  } else {
+    // No input, stop all motors
+    motor_fl.setSpeed(0);
+    motor_fr.setSpeed(0);
+    motor_rl.setSpeed(0);
+    motor_rr.setSpeed(0);
+  }
+}
+
+*/
--- readme.md
+++ readme.md
@@ -1,13 +1,15 @@
 # DIY robot platform
 
 # BOM
-- Arduino Giga R1
+- [arduino](https://docs.arduino.cc/hardware/giga-r1-wifi/)
+- [motor controller](https://www.cytron.io/p-3amp-4v-16v-dc-motor-driver-2-channels)
 
 # ToDo
 - Distance sensors
 - Remote control
 - Battery capacity meter and safety off
 - https://developer.nvidia.com/embedded/learn/get-started-jetson-nano-devkit
+- https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/
 
 # Arduino
 ## libs
