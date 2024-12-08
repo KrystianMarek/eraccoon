5dc327e12a84c4febb441cc55238f187287fe664
Krystian Marek <kmarek@nvidia.com>
2024-12-08 13:14:03 +0100

    update

--- code_sync.sh
+++ code_sync.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -ar arduino "${ER_SSH_USER}@${ER_JETSON_IP}:~/"
+rsync -ar arduino jetson-dualsense \
+  --exclude jetson-dualsense/.idea \
+  --exclude jetson-dualsense/.venv \
+  "${ER_SSH_USER}@${ER_JETSON_IP}:~/"
---
+++ jetson-dualsense/readme.md
@@ -0,0 +1,9 @@
+# dualsense interface
+
+https://github.com/flok/pydualsense
+
+```shell
+sudo cp 70-ps5-controller.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+```
